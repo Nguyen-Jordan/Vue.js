1f073727c8fde105fd9bc6b4519b800eb0eeb354
Nguyen-Jordan <dizorder3@gmail.com>
2023-09-26 16:18:27 +0200

    fin étape 3

---
+++ src/views/CitiesList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="citiesList">
+        <h1>Météo - Liste des villes</h1>
+    </div>
+</template>
+  
+<style>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
+  
